d55773e3b358915142d94fd0ab8b6b5808022b82
Philip Dengler <philip.dengler@novatec-gmbh.de>
2020-12-13 11:26:54 +0100

    drilldown design

--- src/UIElement/Drilddown.tsx
+++ src/UIElement/Drilddown.tsx
@@ -9,7 +9,54 @@ export const Drilldown = ({ }: Props) => {
     return (
         <div style={{ textAlign: "center" }}>
             <label style={{ color: "white", fontSize: 17, paddingTop: "2rem" }}>Container</label>
-            <hr style={{borderTop: "1px solid black", width:"75%", marginTop: "-0.2rem"}}></hr>
+            <hr style={{ borderTop: "1px solid black", width: "75%", marginTop: "-0.1rem" }}></hr>
+            <table style={{ width: "99%" }}>
+                <tr style={{ borderTop: "1px solid black", borderBottom: "1px solid black", backgroundColor: "#505050", color: "#33b5e5" }}>
+                    <th style={{ padding: "5px 5px" }}>Name</th>
+                    <th style={{ padding: "3px 5px" }}>Value</th>
+                </tr>
+                <tr style={{ textAlign:"left" }}>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        Namespace
+                    </td>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        -
+                    </td>
+                </tr>
+                <tr style={{ textAlign:"left"}}>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        Deployment
+                    </td>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        -
+                    </td>
+                </tr>
+                <tr style={{ textAlign:"left" }}>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        Pod
+                    </td>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        -
+                    </td>
+                </tr>
+                <tr style={{ textAlign:"left" }}>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        Container
+                    </td>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        -
+                    </td>
+                </tr>
+                <tr style={{ textAlign:"left" }}>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        Node
+                    </td>
+                    <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
+                        -
+                    </td>
+                </tr>
+            </table>
+
         </div>
     );
 
