1776212ebab45765974e9e07bce2fc69c97ba50d
Philip Dengler <philip.dengler@novatec-gmbh.de>
2020-12-13 14:13:32 +0100

    icon added

--- src/UIElement/Drilddown.tsx
+++ src/UIElement/Drilddown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 type Props = {
 
@@ -6,8 +6,23 @@ type Props = {
 }
 export const Drilldown = ({ }: Props) => {
 
+
+    const [image, setImage] = useState(new window.Image);
+
+    useEffect(() => {
+        const temp = new window.Image;
+        temp.src = "https://raw.githubusercontent.com/fylip97/Thesis/main/src/img/back.png";
+        temp.onload = () => {
+            setImage(temp)
+
+        };
+    });
+
+
+
     return (
         <div style={{ textAlign: "center" }}>
+            <img src={image.src} width={10} height={10} />
             <label style={{ color: "white", fontSize: 17, paddingTop: "2rem" }}>Container</label>
             <hr style={{ borderTop: "1px solid black", width: "75%", marginTop: "-0.1rem" }}></hr>
             <table style={{ width: "99%" }}>
@@ -15,7 +30,7 @@ export const Drilldown = ({ }: Props) => {
                     <th style={{ padding: "5px 5px" }}>Name</th>
                     <th style={{ padding: "3px 5px" }}>Value</th>
                 </tr>
-                <tr style={{ textAlign:"left" }}>
+                <tr style={{ textAlign: "left" }}>
                     <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
                         Namespace
                     </td>
@@ -23,7 +38,7 @@ export const Drilldown = ({ }: Props) => {
                         -
                     </td>
                 </tr>
-                <tr style={{ textAlign:"left"}}>
+                <tr style={{ textAlign: "left" }}>
                     <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
                         Deployment
                     </td>
@@ -31,7 +46,7 @@ export const Drilldown = ({ }: Props) => {
                         -
                     </td>
                 </tr>
-                <tr style={{ textAlign:"left" }}>
+                <tr style={{ textAlign: "left" }}>
                     <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
                         Pod
                     </td>
@@ -39,7 +54,7 @@ export const Drilldown = ({ }: Props) => {
                         -
                     </td>
                 </tr>
-                <tr style={{ textAlign:"left" }}>
+                <tr style={{ textAlign: "left" }}>
                     <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
                         Container
                     </td>
@@ -47,7 +62,7 @@ export const Drilldown = ({ }: Props) => {
                         -
                     </td>
                 </tr>
-                <tr style={{ textAlign:"left" }}>
+                <tr style={{ textAlign: "left" }}>
                     <td style={{ padding: "5px 5px", borderBottom: "1px solid black" }}>
                         Node
                     </td>
