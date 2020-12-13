8bc18ebfbd50f02e59444687904d686b9911b216
Philip Dengler <philip.dengler@novatec-gmbh.de>
2020-12-13 14:49:27 +0100

    style added

--- src/UIElement/Drilddown.tsx
+++ src/UIElement/Drilddown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 type Props = {
 
@@ -7,23 +7,13 @@ type Props = {
 export const Drilldown = ({ }: Props) => {
 
 
-    const [image, setImage] = useState(new window.Image);
-
-    useEffect(() => {
-        const temp = new window.Image;
-        temp.src = "https://raw.githubusercontent.com/fylip97/Thesis/main/src/img/back.png";
-        temp.onload = () => {
-            setImage(temp)
-
-        };
-    });
-
-
-
     return (
         <div style={{ textAlign: "center" }}>
-            <img src={image.src} width={10} height={10} />
-            <label style={{ color: "white", fontSize: 17, paddingTop: "2rem" }}>Container</label>
+            <div style={{width: "99%", paddingTop:"1rem"}}>
+                <img src="https://raw.githubusercontent.com/fylip97/Thesis/main/src/img/back2.png" style={{ width: "25px", height: "25px" , float:"right", marginRight: "1rem"}} />
+            </div>
+            <label style={{ color: "white", fontSize: 17, paddingTop: "1.5rem" }}>Container</label>
+            <img src="https://raw.githubusercontent.com/fylip97/Thesis/main/src/img/metrics.png" style={{width: "25px", height: "25px", marginLeft:"1rem", marginBottom: "0.55rem"}}/>
             <hr style={{ borderTop: "1px solid black", width: "75%", marginTop: "-0.1rem" }}></hr>
             <table style={{ width: "99%" }}>
                 <tr style={{ borderTop: "1px solid black", borderBottom: "1px solid black", backgroundColor: "#505050", color: "#33b5e5" }}>
