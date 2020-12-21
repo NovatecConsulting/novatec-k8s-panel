362a688b8277db17e776f04a1b3d9063235cdc3a
Philip Dengler <philip.dengler@novatec-gmbh.de>
2020-12-21 17:23:32 +0100

    update metric

--- src/UIElement/DropdownOptions.tsx
+++ src/UIElement/DropdownOptions.tsx
@@ -1,5 +1,5 @@
 import { PanelData, SelectableValue } from '@grafana/data';
-import { getNamespaceInformation, getDeploymentInformation, getContainerInformation, getPodInformation, } from '../processMetric/ConvertData'
+import { getAllElementInfo } from '../processMetric/ConvertData'
 
 export function dropdownOptions(allOptions: string[], value: string) {
 
@@ -61,43 +61,35 @@ export function dropdownOptionsFilter(data: PanelData, value: string | undefined
     }
 
     if (levelOption !== "Overview") {
-        //all Namespaces
-        let item = getNamespaceInformation(data);
+        const allElement = getAllElementInfo(data);
 
-        for (let i = 0; i < item.length; i++) {
-            let element: SelectableValue = {};
-            element.label = item[i].namespace;
-            element.description = "Namespace";
-            option.push(element);
+        for (let i = 0; i < allElement.length; i++) {
+            let namespaceElement: SelectableValue = {};
+            namespaceElement.label = allElement[i].Name;
+            namespaceElement.description = "Namespace";
+            option.push(namespaceElement);
         }
 
-        //all Services
-        item = getDeploymentInformation(data);
+        for (let i = 0; i < allElement.length; i++) {
+            for (let l = 0; l < allElement[i].Pod.length; l++) {
+                let podElement: SelectableValue = {};
+                podElement.label = allElement[i].Pod[l].Name;
+                podElement.description = "Pod";
 
-        for (let i = 0; i < item.length; i++) {
-            let element: SelectableValue = {};
-            element.label = item[i].deployment;
-            element.description = "Deployment";
-            option.push(element);
-        }
-        //all Pods
-        item = getPodInformation(data);
-
-        for (let i = 0; i < item.length; i++) {
-            let element: SelectableValue = {};
-            element.label = item[i].pod;
-            element.description = "Pod";
-            option.push(element);
+                option.push(podElement)
+            }
         }
 
-        //all Containers
-        item = getContainerInformation(data);
+        for (let i = 0; i < allElement.length; i++) {
+            for (let l = 0; l < allElement[i].Pod.length; l++) {
+                for (let j = 0; j < allElement[i].Pod[l].Container.length; j++) {
+                    let containerElement: SelectableValue = {};
+                    containerElement.label = allElement[i].Pod[l].Container[j].Name;
+                    containerElement.description = "Container";
 
-        for (let i = 0; i < item.length; i++) {
-            let element: SelectableValue = {};
-            element.label = item[i].container
-            element.description = "Container";
-            option.push(element);
+                    option.push(containerElement);
+                }
+            }
         }
     }
     return option;
--- src/processMetric/ConvertData.tsx
+++ src/processMetric/ConvertData.tsx
@@ -5,30 +5,12 @@ import { Types, Container, Pod, Namespace } from 'types';
 
 export function getDeploymentCount(data: PanelData) {
 
-    return data.series[3].fields[1].values.get(0);
+    return data.series[1].fields[1].values.get(0);
 }
-
-
-
-
-// Name
-// Namespace
-export function getNamespaceInformation(data: PanelData) {
-
-    const allNames = data.series[6].fields[1].values;
-    let allElementInfo = new Array();
-
-    for (let i = 0; i < allNames.length; i++) {
-        let elementInfo = { type: Types.Namespace, container: "", pod: "", namespace: allNames.get(i), node: "", deployment: "" }
-        allElementInfo.push(elementInfo);
-    }
-    return allElementInfo;
-}
-
 
 // Deployment
 export function getDeploymentInformation(data: PanelData) {
-    const allNames = data.series[7].fields[1].values;
+    const allNames = data.series[2].fields[1].values;
     let allElementInfo = new Array();
     for (let i = 0; i < allNames.length; i++) {
         let elementInfo = { type: Types.Deployment, container: "", pod: "", namespace: "", node: "", deployment: allNames.get(i) }
@@ -37,67 +19,25 @@ export function getDeploymentInformation(data: PanelData) {
     return allElementInfo
 }
 
-
-// Pod
-export function getPodInformation(data: PanelData) {
-
-    const allNames = data.series[4].fields[1].values;
-    let allElementInfo = new Array();
-    for (let i = 0; i < allNames.length; i++) {
-        let elementInfo = { type: Types.Pod, container: "", pod: allNames.get(i), namespace: "", node: "", deployment: "" }
-        allElementInfo.push(elementInfo);
-    }
-    return allElementInfo;
-}
-
-// Container
-export function getContainerInformation(data: PanelData) {
-
-    const allNames = data.series[5].fields[1].values;
-    let allElementInfo = new Array();
-    for (let i = 0; i < allNames.length; i++) {
-        let elementInfo = { type: Types.Container, container: allNames.get(i), pod: "", namespace: "", node: "", deployment: "" }
-        allElementInfo.push(elementInfo);
-    }
-    return allElementInfo;
-}
-
-
-
-
-
 export function getAllContainer(data: PanelData) {
 
     let allElementInfo = new Array();
-    for (let i = 0; i < data.series[8].fields[5].values.length; i++) {
+    for (let i = 0; i < data.series[0].fields[5].values.length; i++) {
         let elementInfo = { type: Types.Container, container: "", pod: "", namespace: "", node: "", deployment: "" };
 
-        elementInfo.container = data.series[8].fields[5].values.get(i);
-        elementInfo.pod = data.series[8].fields[16].values.get(i);
-        elementInfo.namespace = data.series[8].fields[15].values.get(i);
+        elementInfo.container = data.series[0].fields[5].values.get(i);
+        elementInfo.pod = data.series[0].fields[16].values.get(i);
+        elementInfo.namespace = data.series[0].fields[15].values.get(i);
         allElementInfo.push(elementInfo);
     }
     return allElementInfo;
-
 }
 
 
 
 export function getAllElementInfo(data: PanelData) {
 
-
-    let allElementInfo = new Array();
-
-    for (let i = 0; i < data.series[8].fields[5].values.length; i++) {
-        let elementInfo = { type: Types.Container, container: "", pod: "", namespace: "", node: "", deployment: "" };
-
-        elementInfo.container = data.series[8].fields[5].values.get(i);
-        elementInfo.pod = data.series[8].fields[16].values.get(i);
-        elementInfo.namespace = data.series[8].fields[15].values.get(i);
-        allElementInfo.push(elementInfo);
-    }
-
-
+    const allElementInfo = getAllContainer(data);
     let allContainers: Container[] = [];
 
     for (let i = 0; i < allElementInfo.length; i++) {
@@ -107,9 +47,14 @@ export function getAllElementInfo(data: PanelData) {
 
     let allPods: Pod[] = [];
 
-    let allElementPod = getPodInformation(data);
+    let podSet = new Set();
+    for (let i = 0; i < allElementInfo.length; i++) {
+        podSet.add(allElementInfo[i].pod);
+    }
+    let allElementPod = Array.from(podSet);
+    console.log(allElementPod);
     for (let i = 0; i < allElementPod.length; i++) {
-        let pod: Pod = { Name: allElementPod[i].pod, Container: [], Namespace: "" }
+        let pod: Pod = { Name: "" + allElementPod[i], Container: [], Namespace: "" }
         for (let l = 0; l < allContainers.length; l++) {
             if (allContainers[l].Pod === pod.Name) {
                 pod.Container.push(allContainers[l]);
@@ -119,14 +64,15 @@ export function getAllElementInfo(data: PanelData) {
         allPods.push(pod);
     }
 
-
     let allNamespaces: Namespace[] = [];
 
-    let allElementNamespace = getNamespaceInformation(data);
-
-
+    let namespaceSet = new Set();
+    for (let i = 0; i < allElementInfo.length; i++) {
+        namespaceSet.add(allElementInfo[i].namespace);
+    }
+    let allElementNamespace = Array.from(namespaceSet);
     for (let i = 0; i < allElementNamespace.length; i++) {
-        let namespace: Namespace = { Name: allElementNamespace[i].namespace, Pod: [] }
+        let namespace: Namespace = { Name: "" + allElementNamespace[i], Pod: [] }
         for (let l = 0; l < allPods.length; l++) {
             if (allPods[l].Namespace === namespace.Name) {
                 namespace.Pod.push(allPods[l])
@@ -134,8 +80,5 @@ export function getAllElementInfo(data: PanelData) {
         }
         allNamespaces.push(namespace);
     }
-
-
     return allNamespaces;
-
 }
